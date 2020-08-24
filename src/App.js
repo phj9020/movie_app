52b80e6dd6672e28c7258db67b5e478630580178
HanJin Park <phj9020@hotmail.com>
2020-08-24 20:57:59 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello1</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="sangiosal" />
+      <Food fav="chukkumi" />
+      {/* Food 컴포넌트에 fav 이라는 prop (property) 을 kimchi라는 값을 준거다 */}
     </div>
   );
 }
