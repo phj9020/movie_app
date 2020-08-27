14e0c9f8b66e7c5af0d550f0cda45350f0a74a83
HanJin Park <phj9020@hotmail.com>
2020-08-27 15:00:42 +0900

    Updates

---
+++ precache-manifest.6a9b04dd78f0b50ca5d9f990962b736d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a802cdccafea8cc75d20e7eb6ac80ef",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fc32a2bfe4a496ccb188",
+    "url": "/movie_app/static/css/main.e635aa32.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc32a2bfe4a496ccb188",
+    "url": "/movie_app/static/js/main.3e0b6e31.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
