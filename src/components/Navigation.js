f94fdccb9d1a6c43377a6316755a09982c1a2c3e
HanJin Park <phj9020@hotmail.com>
2020-08-27 18:23:01 +0900

    navi css added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <div>
+    <div className="navi">
       {/* <a href="/">Home</a> 로 하게 되면 클릭시 페이지가 리프레쉬 되면서 리엑트가 죽는 문제가 있으므로 <Link t0>사용*/}
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/" className="navi__home">Home</Link>
+      <Link to="/about" className="navi__about">About</Link>
     </div>
   );
 }
